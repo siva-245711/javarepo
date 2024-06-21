ca7b288215127cc233d2aced95f47111543a43a1
siva-245711 <sivanagendra809@gmail.com>
2024-06-21 18:10:55 +0530

    dowhile

---
+++ day_1/src/Whileloop.java
@@ -0,0 +1,12 @@
+public class Whileloop{
+    public static void main(String[] args) {
+        int count=5;
+        while (count>0) 
+            
+        
+        {
+        System.out.println("numbers"+ count );
+        count--;
+    }
+}
+}
---
+++ day_1/src/dowhile.java
@@ -0,0 +1,10 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int docount =0;
+        do { 
+            System.out.println("num"+docount);
+            docount++;
+        } while (docount<10);
+    }
+    
+}
